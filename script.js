6c18d5d200da2d36d833805e142d13039c2cc0ce
Amir <mehdizadeh.amir11@gmail.com>
2023-02-28 21:34:04 +0330

    adding task completed

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const sumbitBtn = document.getElementById("submit");
-sumbitBtn.addEventListener("click",addTask(e));
-var txtInput ;
-function addTask (e){
-    txtInput = document.getElementById("text").value; 
-    console.log(e.target.value)
+sumbitBtn.addEventListener("click", addTask);
+function addTask(event) {
+  event.preventDefault();
+  //adding div
+  const textInput = document.getElementById("text");
+  const taskHolder = document.querySelector(".task-holder");
+  //creating an element
+  const newTask = document.createElement("p");
+  newTask.innerText = textInput.value;
+  console.log(newTask)
+  //apending to task holder
+  taskHolder.appendChild(newTask);
 }
