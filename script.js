67d57efb3a91ec93268e9855fe0a8d4c1d2ad762
Amir <mehdizadeh.amir11@gmail.com>
2023-03-10 22:13:20 +0330

    local storage added

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
+const taskes = [];
 const sumbitBtn = document.getElementById("submit");
 sumbitBtn.addEventListener("click", addTask);
+
 function addTask(event) {
   event.preventDefault();
-  //adding div
   const textInput = document.getElementById("text");
+  if (textInput.value === "" || textInput.value === null) {
+    alert("empty task not allowed");
+  } else {
+    taskes.push(textInput.value);
+    localStorage.setItem("tasks", JSON.stringify(taskes));
+  }
+}
+
+function creatingTask() {
   const taskHolder = document.querySelector(".task-holder");
-  //creating an element
   const newTask = document.createElement("p");
-  newTask.innerText = textInput.value;
-  console.log(newTask)
-  //apending to task holder
-  taskHolder.appendChild(newTask);
+  const createdTasks = JSON.parse(localStorage.getItem("tasks"));
+  
 }
