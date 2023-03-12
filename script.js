078cfa7222d1fe6aa46877a7561a71cba8c3c6a1
mahdizadehamir <mehdizadeh.amir11@gmail.com>
2023-03-12 23:33:39 +0330

    adding taskes completed

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
-const taskes = [];
+//defining items
+const tasks = JSON.parse(localStorage.getItem("todoList")) || [];
 const sumbitBtn = document.getElementById("submit");
 sumbitBtn.addEventListener("click", addTask);
-
+const taskHolder = document.querySelector(".task-holder");
+//defining functions
 function addTask(event) {
   event.preventDefault();
   const textInput = document.getElementById("text");
   if (textInput.value === "" || textInput.value === null) {
     alert("empty task not allowed");
   } else {
-    taskes.push(textInput.value);
-    localStorage.setItem("tasks", JSON.stringify(taskes));
+    tasks.push(textInput.value);
+    displayTasks(textInput.value);
+    localStorage.setItem("todoList", JSON.stringify(tasks));
+    textInput.value = ""
   }
 }
-
-function creatingTask() {
-  const taskHolder = document.querySelector(".task-holder");
-  const newTask = document.createElement("p");
-  const createdTasks = JSON.parse(localStorage.getItem("tasks"));
-  
+//showing Tasks
+function displayTasks(inputValue) {
+  if (tasks != null || []) {
+    const newEl = document.createElement("p");
+    newEl.setAttribute("id","task-box")
+    newEl.textContent = inputValue;
+    taskHolder.appendChild(newEl);
+  }
 }
+function initialShowingTasks(){
+  for (const element of tasks){
+    displayTasks(element)
+  }
+}
+initialShowingTasks()
