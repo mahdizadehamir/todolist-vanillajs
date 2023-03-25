539aa11605f2d97347c3966e1e274c69f97571b7
Amir <mehdizadeh.amir11@gmail.com>
2023-03-25 22:19:30 +0330

    so unclearly added! all function (needing refactor)

--- script.js
+++ script.js
@@ -1,11 +1,43 @@
 //defining items
 const tasks = JSON.parse(localStorage.getItem("todoList")) || [];
-const completedTask = JSON.parse(localStorage.getItem("completed")) || [];
+const completedTask = JSON.parse(localStorage.getItem("completedTask")) || [];
 const sumbitBtn = document.getElementById("submit");
 sumbitBtn.addEventListener("click", addTask);
 const taskHolder = document.querySelector(".task-holder");
 const textInput = document.getElementById("text");
+const selectButton = document.getElementById("select");
+selectButton.addEventListener("click",changingValue);
+
+
+
+
 //defining functions
+function changingValue(event){
+  if(event.target.value === "completed"){
+      for (element of completedTask){
+        taskHolder.innerHTML = "";
+        displayTasks(element)
+      }
+  }else if(event.target.value === "uncompleted"){
+    const filteredValues = tasks.filter(function(value){
+      if(completedTask.includes(value) === false){
+        return value
+      }
+    })
+    for (element of filteredValues){
+      taskHolder.innerHTML = "";
+      displayTasks(element)
+    }
+  }
+  else {
+    taskHolder.innerHTML = "";
+    initialShowingTasks();
+    }
+}
+
+
+
+
 function addTask(event) {
   event.preventDefault();
   if (textInput.value === "" || textInput.value === null) {
@@ -28,23 +60,22 @@ function deletefunc(event) {
     completedTask.indexOf(event.target.parentElement.previousSibling.innerText),
     1
   );
-  console.log(tasks,completedTask);
   localStorage.setItem("todoList", JSON.stringify(tasks));
+  localStorage.setItem("completedTask",JSON.stringify(completedTask));
   taskHolder.innerHTML = "";
   initialShowingTasks();
 }
 //completebutton function
 function completefunc(event) {
-  console.log(event.target.disabled);
   event.target.disabled = true;
   event.target.parentElement.previousSibling.innerHTML = `<del>${event.target.parentElement.previousSibling.innerText}</del>`;
   completedTask.push(event.target.parentElement.previousSibling.innerText);
-  localStorage.setItem("completedTask",JSON.stringify(completedTask))
-  console.log(completedTask)
+  localStorage.setItem("completedTask", JSON.stringify(completedTask));
 }
 //showing Tasks
 function displayTasks(inputValue) {
   if (tasks != null || []) {
+    console.log(completedTask);
     const deleteBtn = document.createElement("button");
     deleteBtn.textContent = "Delete";
     const completeBtn = document.createElement("button");
@@ -53,7 +84,11 @@ function displayTasks(inputValue) {
     const btnHolder = document.createElement("div");
     const paragraph = document.createElement("p");
     newEl.setAttribute("id", "task-box");
-    paragraph.textContent = inputValue;
+    if (completedTask.includes(inputValue)) {
+      paragraph.innerHTML = `<del>${inputValue}</del>`;
+    } else {
+      paragraph.textContent = inputValue;
+    }
     deleteBtn.addEventListener("click", deletefunc);
     completeBtn.addEventListener("click", completefunc);
     btnHolder.appendChild(deleteBtn);
